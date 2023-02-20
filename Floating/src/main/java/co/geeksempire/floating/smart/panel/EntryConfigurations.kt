6d7bf6165dbf4895b3253ee7ab63972ba429bb45
Elias Fazel <elias.fazel@gmail.com>
2023-02-20 06:22:23 -0800

    [Description]
    [ChangeLog]
    ~ Initializing
    
    [Build][ChangeLog]
    # Dependencies Updated

--- Floating/src/main/java/co/geeksempire/floating/smart/panel/EntryConfigurations.kt
+++ Floating/src/main/java/co/geeksempire/floating/smart/panel/EntryConfigurations.kt
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 2/18/23, 9:02 AM
+ * Last modified 2/20/23, 6:07 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -12,10 +12,19 @@ package co.geeksempire.floating.smart.panel
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import co.geeksempire.floating.smart.panel.databinding.EntryConfigurationsLayoutBinding
 
 class EntryConfigurations : AppCompatActivity() {
+
+    lateinit var entryConfigurationsLayoutBinding: EntryConfigurationsLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        entryConfigurationsLayoutBinding = EntryConfigurationsLayoutBinding.inflate(layoutInflater)
+        setContentView(entryConfigurationsLayoutBinding.root)
+
+
+
     }
+
 }
