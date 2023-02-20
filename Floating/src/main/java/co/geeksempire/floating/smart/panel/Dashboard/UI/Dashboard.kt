6260510b5b8977c7f1bdbc86e94c2b6b23021efa
Elias Fazel <elias.fazel@gmail.com>
2023-02-20 08:02:29 -0800

    [Description]
    [ChangeLog]
    ~ Initializing
    
    [Build][ChangeLog]
    # Dependencies Updated

--- Floating/src/main/java/co/geeksempire/floating/smart/panel/Dashboard/UI/Dashboard.kt
+++ Floating/src/main/java/co/geeksempire/floating/smart/panel/Dashboard/UI/Dashboard.kt
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 2/20/23, 6:33 AM
+ * Last modified 2/20/23, 8:01 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -12,6 +12,7 @@ package co.geeksempire.floating.smart.panel.Dashboard.UI
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import co.geeksempire.floating.smart.panel.R
 import co.geeksempire.floating.smart.panel.databinding.DashboardLayoutBinding
 
 class Dashboard : AppCompatActivity() {
@@ -22,6 +23,10 @@ class Dashboard : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         dashboardLayoutBinding = DashboardLayoutBinding.inflate(layoutInflater)
         setContentView(dashboardLayoutBinding.root)
+
+        window.decorView.setBackgroundColor(getColor(R.color.black))
+
+
     }
 
 }
