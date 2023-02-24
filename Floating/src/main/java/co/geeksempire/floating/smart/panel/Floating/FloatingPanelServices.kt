2813f3d1b3bed6e7d433264f98b0804e97d0e0ca
Elias Fazel <elias.fazel@gmail.com>
2023-02-24 09:26:36 -0800

    [Description]
    [ChangeLog]
    ~ Dashboard User Interface -> Switches Completed
    
    [Build][ChangeLog]
    # Dependencies Updated

---
+++ Floating/src/main/java/co/geeksempire/floating/smart/panel/Floating/FloatingPanelServices.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2023 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 2/24/23, 9:26 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.floating.smart.panel.Floating
+
+// start foreground service
+// add to manifest
+class FloatingPanelServices {
+}
