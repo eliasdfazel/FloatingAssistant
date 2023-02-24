3a06c16d4b2d35aa4a455b6dd76a8ec431fb27e9
Elias Fazel <elias.fazel@gmail.com>
2023-02-24 09:04:10 -0800

    [Description]
    [ChangeLog]
    ~ Dashboard User Interface -> Switches Completed
    
    [Build][ChangeLog]
    # Dependencies Updated

--- Floating/src/main/java/co/geeksempire/floating/smart/panel/Dashboard/Extensions/UserInterface.kt
+++ Floating/src/main/java/co/geeksempire/floating/smart/panel/Dashboard/Extensions/UserInterface.kt
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 2/24/23, 8:57 AM
+ * Last modified 2/24/23, 9:02 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -175,5 +175,6 @@ fun Dashboard.setupUserInterface() {
 
     })
     /* End - Usage Access  */
+
 
 }
