1e932ab31c0a39921c4a5e52f8c3546a507669e4
Elias Fazel <elias.fazel@gmail.com>
2023-03-03 06:19:30 -0800

    [Description]
    [ChangeLog]
    ~ Database Queries Added
    
    [Build][ChangeLog]
    # Dependencies Updated

--- Floating/src/main/java/co/geeksempire/floating/smart/panel/Tests/PrototypeData.kt
+++ Floating/src/main/java/co/geeksempire/floating/smart/panel/Tests/PrototypeData.kt
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 3/3/23, 5:40 AM
+ * Last modified 3/3/23, 6:19 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -18,18 +18,18 @@ import co.geeksempire.floating.smart.panel.Database.ArwenDataStructure
 import co.geeksempire.floating.smart.panel.Database.Database
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class PrototypeData (private val context: Context) {
 
-    fun generate() = CoroutineScope(Dispatchers.IO).async {
+    fun generate() = CoroutineScope(Dispatchers.IO).launch {
 
         val arwenDataInterface = Room.databaseBuilder(context, ArwenDataInterface::class.java, Database.DatabaseName)
 
         val arwenDatabaseAccess = arwenDataInterface.build().initializeDataAccessObject()
 
-        for (i in 0..10000) {
+        for (i in 0..2000) {
 
             delay(13)
 
@@ -46,8 +46,15 @@ class PrototypeData (private val context: Context) {
 
             arwenDatabaseAccess.insert(arwenDataStructure)
 
-            Log.d(this@PrototypeData.javaClass.simpleName, arwenDataStructure.toString())
+            Log.d(this@PrototypeData.javaClass.simpleName, "Index: $i")
         }
+
+        delay(3579)
+
+        Log.d(this@PrototypeData.javaClass.simpleName, "Database Finished")
+
+        val databaseIndexes = arwenDatabaseAccess.rowCount()
+        Log.d(this@PrototypeData.javaClass.simpleName, "Row Count; $databaseIndexes")
 
     }
 
