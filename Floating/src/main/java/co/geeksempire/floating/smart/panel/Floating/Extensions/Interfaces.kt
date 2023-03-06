01d3dfe5be270091afff8a4a1117bd08bade2807
Elias Fazel <elias.fazel@gmail.com>
2023-03-06 07:58:21 -0800

    [Description]
    [ChangeLog]
    ~ Database Queries Added
    
    [Build][ChangeLog]
    # Dependencies Updated

--- Floating/src/main/java/co/geeksempire/floating/smart/panel/Floating/Extensions/Interfaces.kt
+++ Floating/src/main/java/co/geeksempire/floating/smart/panel/Floating/Extensions/Interfaces.kt
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 3/6/23, 7:10 AM
+ * Last modified 3/6/23, 7:57 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -33,7 +33,7 @@ fun FloatingPanelServices.registerFloatingBroadcasts(floatingLayoutBinding: Floa
                 if (intent.action == ColorsIO.Type.colorsChanged) {
                     Log.d(this@registerFloatingBroadcasts.javaClass.simpleName, "New Background Color Received")
 
-                    val backgroundColor = setColorAlpha(intent.getIntExtra(ColorsIO.Type.dominantColor, 0), 1f)
+                    val backgroundColor = setColorAlpha(intent.getIntExtra(ColorsIO.Type.dominantColor, 0), 173f)
 
                     floatingLayoutBinding.rootView.setBackgroundColor(backgroundColor)
 
